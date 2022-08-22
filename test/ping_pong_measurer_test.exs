8b3b0ad8ae2533547541b5b6e91be1fef2285220
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-08-22 12:24:50 +0900

    Improve PingPongMeasurerTest

--- test/ping_pong_measurer_test.exs
+++ test/ping_pong_measurer_test.exs
@@ -1,16 +1,18 @@
 defmodule PingPongMeasurerTest do
   use ExUnit.Case
 
-  for process_count <- [1, 10, 100] do
+  for process_count <- [1, 10, 100], payload_bytes <- [10, 100, 1000, 10000] do
     @tag :tmp_dir
     @tag :capture_log
-    test "start_(pong|ping)_process with process_count: #{process_count}", %{tmp_dir: tmp_dir} do
+    test "start_(pong|ping)_process with process_count: #{process_count}, payload_bytes: #{payload_bytes}",
+         %{tmp_dir: tmp_dir} do
       process_count = unquote(process_count)
+      payload_bits = 8 * unquote(payload_bytes)
 
       PingPongMeasurer.start_pong_processes(process_count)
       PingPongMeasurer.start_ping_processes(tmp_dir, process_count)
 
-      PingPongMeasurer.ping()
+      PingPongMeasurer.ping(process_count, <<1::size(payload_bits)>>)
 
       PingPongMeasurer.stop_pong_processes()
       PingPongMeasurer.stop_ping_processes()
